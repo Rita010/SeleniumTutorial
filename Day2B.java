d6316a5ca5098f24644d566136463590fdef7886
Rita010 <ritakhatiwada50@gmail.com>
2023-08-25 01:11:12 -0700

    first commit

--- Day2B.java
+++ Day2B.java
@@ -4,39 +4,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Day2 {
+public class Day2B {
 
 	public static void main(String[] args) {
+
+		//path 
 		
-	//path 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ritak\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
 		ChromeDriver dr = new ChromeDriver();
 		
 		dr.get("https://www.saucedemo.com/");
+			
+		WebElement username = dr.findElement(By.cssSelector("#user-name"));
+		WebElement password = dr.findElement(By.cssSelector("#password"));
+		WebElement login = dr.findElement(By.cssSelector("#login-button"));
 		
-		WebElement UserName = dr.findElement(By.cssSelector("#user-name"));
-		
-		WebElement Password = dr.findElement(By.cssSelector("#password"));
-		
-		WebElement Login = dr.findElement(By.cssSelector("#login-button"));
-		
-		
-		UserName.sendKeys("standard_user");
-		Password.sendKeys("secret_sauce");
-		Login.click();
-		
-		WebElement heading = dr.findElement(By.cssSelector("#header_container > div.header_secondary_container > span "));
-		
+		username.sendKeys("standard_user");
+		password.sendKeys("secret_sauce");
+		login.click();
+
+		WebElement heading = dr.findElement(By.cssSelector("#header_container > div.header_secondary_container > span"));
+
 		if(heading.isDisplayed()) {
 			System.out.println("Test case passed");
-			
 		}
-		else
-		{
+		else {
 			System.out.println("Test case failed");
-		}	
+		}
 		
 	}
 
 }
-
--- Day2bAllCodeCopiedFromSir.java
+++ Day2bAllCodeCopiedFromSir.java
@@ -11,30 +11,49 @@ public class Day2bAllCodeCopiedFromSir {
 		
 		
 		
-		 // cntrOl + shift + o
-		//  System.setProperty("webdriver.chrome.driver", "C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver\\chromedriver.exe");
+		 // cntrOl + shift + o : to import
+		
 		//  ChromeDriver driver = new ChromeDriver();
+		
 		//  driver.get("https://www.google.com");
-	  // local dev staging canary production
+		
+//  Environment for Testing :
+		
+	  // local, development, staging, canary, production
 	  // local test test4 test6 production
-	  // Arrange  System.setProperty("webdriver.chrome.driver","C:\\Users\\ritak\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe"); 
+		
+// Arrange 
+		
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\ritak\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe"); 
+		
 		ChromeDriver driver = new ChromeDriver();
-	  // Actions
-	  // Selecting the element
-	  driver.get("https://www.saucedemo.com/");  
+		
+// Actions
+		
+// Selecting the element
+		
+	  driver.get("https://www.saucedemo.com/"); 
+	  
 	  WebElement username = driver.findElement(By.cssSelector("#user-name"));
 	  WebElement password = driver.findElement(By.cssSelector("#password"));  
 	  WebElement login = driver.findElement(By.cssSelector("#login-button"));
-	    // Dash board element
+	  
+// Dash board element
+	  
 	  username.sendKeys("standard_user"); 
 	  password.sendKeys("secret_sauce");
 	  login.click();  
-	  // Assertion
+	  
+// Assertion
+	  
 	  WebElement heading = driver.findElement(By.cssSelector("#header_container > div.header_secondary_container > span"));
-	  if(heading.isDisplayed()) {
-	   System.out.println("Test case pass");  }
-	  else {   System.out.println("Test case fail");
-	  }  
+	  
+	  if(heading.isDisplayed()) {  
+	   System.out.println("Test case pass");  
+	   }
+	  	else { 
+	  		System.out.println("Test case fail");
+	  	}  
 	 
 	}
 
